7db5513b7102c541616acb874d25e273d1e19bce
Madhur Kamble <165660382+madhurkamble@users.noreply.github.com>
2026-07-09 10:34:49 +0530

    Time: 0 ms (100%), Space: 33.1 MB (22.36%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -1,5 +1,14 @@
 class Solution {
 public:
+
+    int gcd(int a, int b) {
+        while (b != 0) {
+        int rem = a % b;
+        a = b;
+        b = rem;
+        }
+        return a;
+    }
     bool isGoodArray(vector<int>& nums) {
        int ans=nums[0];
        for(int i=0;i<nums.size();i++){
